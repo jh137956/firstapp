732b49627d354e32be56f6e95b2c87ef1df03187
[ojh] <[ojh@dotnetsoft.co.kr]>
2022-12-30 01:25:53 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,51 @@
-import React from 'react';
-import { Stack, Text, Link, FontWeights, IStackTokens, IStackStyles, ITextStyles } from '@fluentui/react';
-import logo from './logo.svg';
+import React, { FormEvent, useMemo, useState } from 'react';
+import { Stack, SearchBox, DefaultButton, TextField, DetailsList, Text, IColumn } from '@fluentui/react';
 import './App.css';
 
-const boldStyle: Partial<ITextStyles> = { root: { fontWeight: FontWeights.semibold } };
-const stackTokens: IStackTokens = { childrenGap: 15 };
-const stackStyles: Partial<IStackStyles> = {
-  root: {
-    width: '960px',
-    margin: '0 auto',
-    textAlign: 'center',
-    color: '#605e5c',
-  },
-};
-
 export const App: React.FunctionComponent = () => {
-  return (
-    <Stack horizontalAlign="center" verticalAlign="center" verticalFill styles={stackStyles} tokens={stackTokens}>
-      <img className="App-logo" src={logo} alt="logo" />
-      <Text variant="xxLarge" styles={boldStyle}>
-        Welcome to your Fluent UI app
-      </Text>
-      <Text variant="large">For a guide on how to customize this project, check out the Fluent UI documentation.</Text>
-      <Text variant="large" styles={boldStyle}>
-        Essential links
-      </Text>
-      <Stack horizontal tokens={stackTokens} horizontalAlign="center">
-        <Link href="https://developer.microsoft.com/en-us/fluentui#/get-started/web">Docs</Link>
-        <Link href="https://stackoverflow.com/questions/tagged/office-ui-fabric">Stack Overflow</Link>
-        <Link href="https://github.com/microsoft/fluentui/">Github</Link>
-        <Link href="https://twitter.com/fluentui">Twitter</Link>
+   const [nameValue, setNameValue] = useState<string>();
+   const [mailValue, setMailValue] = useState<string>();
+   const [users, setUsers] = useState<any[]>([]);
+   const [fuser, setfuser] = useState<any[]>([]);
+   const onClickSetState = (name?: string, mail?: string) => {
+      let listColumns2 = [];
+      listColumns2.push({ key: name!, name: mail! });
+      setfuser(listColumns2);
+
+      let listColumns = [];
+      listColumns = users;
+      listColumns.push({ key: name!, name: mail! });
+      setUsers(listColumns);
+   };
+
+   const usersList = useMemo(() => {
+      return (
+         <>
+            {users.map((element: any, index: number) => {
+               return <Stack.Item key={index}>{element.key}</Stack.Item>;
+            })}
+         </>
+      );
+   }, [fuser]);
+
+   return (
+      <Stack horizontalAlign="start" verticalAlign="start">
+         <Stack horizontal>
+            <TextField
+               placeholder="계정명"
+               onChange={(event?: FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string | undefined) =>
+                  setNameValue(newValue)
+               }
+            />
+            <TextField
+               placeholder="메일명"
+               onChange={(event?: FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string | undefined) =>
+                  setMailValue(newValue)
+               }
+            />
+            <DefaultButton text="추가" onClick={() => onClickSetState(nameValue, mailValue)}></DefaultButton>
+         </Stack>
+         <Stack.Item>{usersList}</Stack.Item>
       </Stack>
-      <Text variant="large" styles={boldStyle}>
-        Design system
-      </Text>
-      
-      <Stack horizontal tokens={stackTokens} horizontalAlign="center">
-        <Link href="https://developer.microsoft.com/en-us/fluentui#/styles/web/icons">Icons</Link>
-        <Link href="https://developer.microsoft.com/en-us/fluentui#/styles/web">Styles</Link>
-        <Link href="https://aka.ms/themedesigner">Theme designer</Link>
-      </Stack>
-    </Stack>
-  );
+   );
 };
